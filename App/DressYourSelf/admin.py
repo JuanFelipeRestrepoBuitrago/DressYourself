01fdb978a7ab89aeca11885c522e5f871b141165
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-08-17 21:17:03 -0500

    Models Modified
    I modified models to adjust them to our project and our necessities

--- App/DressYourSelf/admin.py
+++ App/DressYourSelf/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Garment, Outfit
 
 # Register your models here.
+admin.site.register(Garment)
+admin.site.register(Outfit)
+
--- App/DressYourSelf/models.py
+++ App/DressYourSelf/models.py
@@ -1,25 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-
-    def full_name(self):
-        return self.name + " " + self.lastname
-
-    def __str__(self):
-        return self.lastname + " " + self.name
-
-    class Meta:
-        ordering = ['name']
-
-
 class Garment(models.Model):
     class Category(models.TextChoices):
         TOP = 'T', _('Top')
@@ -36,13 +20,13 @@ class Garment(models.Model):
         INTIMATES = 'I', _('Intimates')
         COSTUME = 'C', _('Costume')
     name = models.CharField(max_length=100)
-    image = models.ImageField()
-    description = models.TextField(null=True)
+    image = models.ImageField(upload_to='garments/')
+    description = models.TextField(null=True, blank=True)
     category = models.CharField(max_length=2, choices=Category.choices, default=Category.COSTUME)
-    brand = models.CharField(max_length=100, null=True)
-    size = models.CharField(max_length=100, null=True)
-    color = models.CharField(max_length=100, null=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    brand = models.CharField(max_length=100, null=True, blank=True)
+    size = models.CharField(max_length=100, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -53,9 +37,10 @@ class Garment(models.Model):
 
 class Outfit(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     garments = models.ManyToManyField(Garment)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='outfits/', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
