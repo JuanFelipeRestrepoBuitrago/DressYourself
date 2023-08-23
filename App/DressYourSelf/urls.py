f832b051a6034c49057fdadafd05deb00835aba4
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-08-23 14:10:02 -0500

    Template Created

--- App/DressYourSelf/urls.py
+++ App/DressYourSelf/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('add_garment/', views.add_garment, name='add_garment'),
 ]
--- App/DressYourSelf/views.py
+++ App/DressYourSelf/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Garment, Outfit
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def home(request):
+    return HttpResponse('This is the home page!')
 
 # Create your views here.
+def add_garment(request):
+    if request.method == 'GET':
+        categories = Garment.Category.choices
+        print(categories)
+        return render(request, 'add_garment.html', {
+            'categories': categories
+        })
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        image = request.FILES.get('image')
+        description = request.POST.get('description')
+        category = request.POST.get('category')
+        brand = request.POST.get('brand')
+        size = request.POST.get('size')
+        color = request.POST.get('color')
+        user = User.objects.get(username='root')
+        try:
+            garment = Garment.objects.create(
+                name=name,
+                image=image,
+                description=description,
+                category=category,
+                brand=brand,
+                size=size,
+                color=color,
+                user=user
+            )
+
+            return HttpResponse('Garment added!')
+        except Exception as e:
+            return HttpResponse('Error adding garment: ' + str(e))
+        
