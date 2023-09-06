ccf0fc8ac9a854247124852d6c37697fde63f86e
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-09-06 09:46:36 -0500

    Edit Working

--- App/DressYourSelf/views.py
+++ App/DressYourSelf/views.py
@@ -103,6 +103,55 @@ def edit_garment(request, id):
             'cssName': '/css/add_garment.css',
             'jsName': '/js/add_garment.js'
         })
+    elif request.method == 'POST':
+        try:
+            if request.POST.get('name') == '' or request.POST.get('name') is None or request.POST.get('name') == ' ':
+                raise IntegrityError('Name is required')
+            else:
+                name = request.POST.get('name')
+            if request.POST.get('category') == '' or request.POST.get('category') is None or request.POST.get(
+                    'category') == ' ':
+                raise IntegrityError('Category is required')
+            else:
+                category = request.POST.get('category')
+            if request.FILES.get('image') is None:
+                image = garment.image
+            else:
+                image = request.FILES.get('image')
+            if request.POST.get('description') == '' or request.POST.get('description') is None or request.POST.get(
+                    'description') == ' ':
+                description = None
+            else:
+                description = request.POST.get('description')
+            if request.POST.get('brand') == '' or request.POST.get('brand') is None or request.POST.get('brand') == ' ':
+                brand = None
+            else:
+                brand = request.POST.get('brand')
+            if request.POST.get('size') == '' or request.POST.get('size') is None or request.POST.get('size') == ' ':
+                size = None
+            else:
+                size = request.POST.get('size')
+            if request.POST.get('color') == '' or request.POST.get('color') is None or request.POST.get('color') == ' ':
+                color = None
+            else:
+                color = request.POST.get('color')
+            user = request.user
+
+            garment.name = name
+            garment.image = image
+            garment.description = description
+            garment.category = category
+            garment.brand = brand
+            garment.size = size
+            garment.color = color
+
+            garment.save()
+
+            return redirect('garments')
+        except IntegrityError as e:
+            messages.error(request, e)
+            return redirect('edit_garment')
+
 
 # transaction.atomic() is used to rollback the database if an error occurs
 @transaction.atomic
