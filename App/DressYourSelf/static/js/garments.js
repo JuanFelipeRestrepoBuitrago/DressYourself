05a507f744dd5dbe1864acb3c3dd62e27dde1aab
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-09-06 21:11:22 -0500

    Filter Garments Function
    An input to filter my garment by keywords was added and is successfully working

--- App/DressYourSelf/static/js/garments.js
+++ App/DressYourSelf/static/js/garments.js
@@ -2,6 +2,7 @@ const detailsContainer = document.querySelector('.garment-detail');
 const closeDetails = document.querySelector('.garment-detail .close-garment-detail');
 const garmentsDetail = document.querySelectorAll('.garment-detail [data-garment-id]');
 const garments = document.querySelectorAll('.garment-card');
+const searchInput = document.querySelector('#search');
 
 function closeDetailsContainer() {
     detailsContainer.classList.add('inactive');
@@ -37,3 +38,25 @@ document.addEventListener('click', (event) => {
 });
 
 closeDetails.addEventListener('click', closeDetailsContainer);
+
+searchInput.addEventListener('input', (event) => {
+    const searchValue = searchInput.value.toLowerCase();
+    console.log(searchValue);
+    garments.forEach(garment => {
+        const garmentName = garment.querySelector('.garment-info [data-garment-name]').getAttribute('data-garment-name').toLowerCase();
+        const garmentCategory = garment.querySelector('.garment-info [data-garment-category]').getAttribute('data-garment-category').toLowerCase();
+        const garmentDescription = garment.querySelector('.garment-info [data-garment-description]').getAttribute('data-garment-description').toLowerCase();
+        const garmentBrand = garment.querySelector('.garment-info [data-garment-brand]').getAttribute('data-garment-brand').toLowerCase();
+        const garmentColor = garment.querySelector('.garment-info [data-garment-color]').getAttribute('data-garment-color').toLowerCase();
+        const garmentSize = garment.querySelector('.garment-info [data-garment-size]').getAttribute('data-garment-size').toLowerCase();
+        console.log(garmentName, garmentCategory, garmentDescription, garmentBrand, garmentColor, garmentSize);
+
+        if (garmentName.includes(searchValue) || garmentCategory.includes(searchValue) || garmentDescription.includes(searchValue) || garmentBrand.includes(searchValue) || garmentColor.includes(searchValue) || garmentSize.includes(searchValue)) {
+            console.log('includes');
+            garment.classList.remove('inactive');
+        } else {
+            console.log('not includes');
+            garment.classList.add('inactive');
+        }
+    });
+});
