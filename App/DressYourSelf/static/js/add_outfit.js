d2ba0d14a2e74ee2ae3e30355987f13c74fbb7eb
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-10-10 18:55:14 -0500

    Close Modal Working

--- App/DressYourSelf/static/js/add_outfit.js
+++ App/DressYourSelf/static/js/add_outfit.js
@@ -12,13 +12,26 @@ function hideModals(modals) {
     });
 }
 
+function closeModal(modal) {
+    modal.classList.add('inactive');
+    modal.close();
+}
+
 const modals = document.querySelectorAll('.modal');
 const modalButtons = document.querySelectorAll('[data-modal-button]');
+const closeButtons = document.querySelectorAll('[data-modal-close]');
 modalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.garments-container').querySelector('.modal');
         modal.showModal();
         hideModals(modals);
         modal.classList.remove('inactive');
+    });
+});
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
     });
 });
