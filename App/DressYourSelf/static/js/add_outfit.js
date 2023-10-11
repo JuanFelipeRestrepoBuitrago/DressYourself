3a21811e8aad77cb6627603b7e38c3735257688b
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-10-11 17:01:51 -0500

    Add and Delete Garment From Form
    The part of adding and deleting a garment in the form for the frontend was successfully implemented

--- App/DressYourSelf/static/js/add_outfit.js
+++ App/DressYourSelf/static/js/add_outfit.js
@@ -17,9 +17,37 @@ function closeModal(modal) {
     modal.close();
 }
 
+function deleteGarment(garment, input, deleteModalGarmentButton, modalGarment, chosenGarments) {
+    modalGarment.classList.remove('chosen');
+    deleteModalGarmentButton.classList.add('inactive');
+    input.value = input.value.replace(garment.dataset.garmentId, '');
+    input.value = input.value.replace(',,', ',');
+    if (input.value[0] === ',') {
+        input.value = input.value.substring(1);
+    } else if (input.value[input.value.length - 1] === ',') {
+        input.value = input.value.substring(0, input.value.length - 1);
+    }
+    chosenGarments.removeChild(garment);
+}
+
+function deleteGarmentEvent(button) {
+    const garment = button.closest('.garment');
+    const chosenGarments = garment.closest('.chosen-garments');
+    const garmentsContainer = chosenGarments.closest('.garments-container');
+    const input = garmentsContainer.previousElementSibling;
+    const modalGarment = garmentsContainer.querySelector(`.modal .garment[data-garment-id="${garment.dataset.garmentId}"]`);
+    const deleteModalGarmentButton = modalGarment.querySelector('.btn-close');
+
+    deleteGarment(garment, input, deleteModalGarmentButton, modalGarment, chosenGarments);
+}
+
 const modals = document.querySelectorAll('.modal');
 const modalButtons = document.querySelectorAll('[data-modal-button]');
 const closeButtons = document.querySelectorAll('[data-modal-close]');
+const modalGarments = document.querySelectorAll('dialog .garments .garment');
+const deleteChosenGarmentButtons = document.querySelectorAll('.chosen-garments .garment .btn-close');
+const deleteFromModalButtons = document.querySelectorAll('.modal .garment .btn-close');
+
 modalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.garments-container').querySelector('.modal');
@@ -33,5 +61,55 @@ closeButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.modal');
         closeModal(modal);
+    });
+});
+
+
+modalGarments.forEach(garment => {
+    garment.addEventListener('click', (event) => {
+        const modal = garment.closest('.modal');
+        const garmentsContainer = modal.closest('.garments-container');
+        const chosenGarments = garmentsContainer.querySelector('.chosen-garments');
+        const input = garmentsContainer.previousElementSibling;
+        const deleteGarmentButton = garment.querySelector('.btn-close');
+
+        if (!garment.classList.contains('chosen') && event.target !== deleteGarmentButton && !deleteGarmentButton.contains(event.target)) {
+            deleteGarmentButton.classList.remove('inactive');
+            garment.classList.add('chosen');
+            if (input.value === '') {
+                input.value = garment.dataset.garmentId;
+            } else {
+                input.value += ',' + garment.dataset.garmentId;
+            }
+            const chosenGarment = garment.cloneNode(true);
+            chosenGarments.appendChild(chosenGarment);
+            closeModal(modal);
+
+            const deleteChosenGarmentButton = chosenGarment.querySelector('.btn-close');
+            deleteChosenGarmentButton.addEventListener('click', () => {
+                deleteGarmentEvent(deleteChosenGarmentButton);
+            });
+        }
+    });
+});
+
+deleteChosenGarmentButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        deleteGarmentEvent(button);
+    });
+});
+
+deleteFromModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalGarment = button.closest('.garment');
+        const modal = modalGarment.closest('.modal');
+        const garmentsContainer = modal.closest('.garments-container');
+        const chosenGarments = garmentsContainer.querySelector('.chosen-garments');
+        const input = garmentsContainer.previousElementSibling;
+        const garment = garmentsContainer.querySelector(`.chosen-garments .garment[data-garment-id="${modalGarment.dataset.garmentId}"]`);
+        const deleteModalGarmentButton = modalGarment.querySelector('.btn-close');
+
+        deleteGarment(garment, input, deleteModalGarmentButton, modalGarment, chosenGarments);
+        closeModal(modal)
     });
 });
