3ffe06ed39808c366a5b7224aa3ed29e3fff2ac4
Tomas Pineda <tomaspinedanaranjo@gmail.com>
2023-10-19 13:45:43 -0500

    mejora templates passwords

--- App/DressYourSelf/urls.py
+++ App/DressYourSelf/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     #modify password
     path("reset_password/", auth_views.PasswordResetView.as_view(template_name="password/password_reset.html"), name="reset_password"),
     path("reset_password_sent/",auth_views.PasswordResetDoneView.as_view(template_name="password/password_reset_sent.html"), name="password_reset_done"),
-    path("reset/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(template_name="password/change_password.html"), name="password_reset_confirm"),
     path("reset_password_complete/",auth_views.PasswordResetCompleteView.as_view(template_name="password/password_reset_done.html"), name="password_reset_complete"), 
-    path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
-    path("password_change/done/",auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+    #path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
+    #path("password_change/done/",auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
 ]
